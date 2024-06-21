f2b50957ebac1c71fd437989d6ba766b0c304b41
lisavincent31 <lisa.vincent31150@gmail.com>
2024-06-21 12:07:49 +0200

    add all parameters for nelmio

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,13 +11,45 @@ use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Contracts\Cache\ItemInterface;
-use Symfony\Contracts\Cache\TagAwareCacheInterface;
-
+use Symfony\Contracts\Cache\TagAwareCacheInterface;    
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 use App\Repository\ProductRepository;
 use App\Entity\Product;
 
 class ProductController extends AbstractController
 {
+    /**
+     * Cette méthode permet de récupérer l'ensemble des téléphones de luxes référencés dans notre API
+     * 
+     * @OA\Response(
+     *      response=200,
+     *      description="Retourne la liste des téléphones",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref=@Model(type=Product::class))
+     *      )
+     * )
+     * @OA\Parameter(
+     *      name="page",
+     *      in="query",
+     *      description="Le numéro de la page que l'on souhaite récupérer.",
+     *      @OA\Schema(type="integer")
+     * )
+     * @OA\Parameter(
+     *      name="limit",
+     *      in="query",
+     *      description="Le nombre d'éléments que l'on veut récupérer",
+     *      @OA\Schema(type="integer")
+     * )
+     * @OA\Tag(name="Products")
+     * 
+     * @param ProductRepository $productRepository
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @return JsonResponse
+     */
     #[Route('/api/products', name: 'products', methods: ['GET'])]
     public function getProductList(ProductRepository $productRepository, 
                                     SerializerInterface $serializer,
@@ -41,6 +73,23 @@ class ProductController extends AbstractController
         return new JsonResponse($products, Response::HTTP_OK, [], true); // return the productList with response 200
     }
 
+    /**
+     * Cette méthode permet de récupérer le détail d'un seul téléphone de luxe référencé dans notre API
+     * 
+     * @OA\Response(
+     *      response=200,
+     *      description="Retourne le détail d'un téléphone",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref=@Model(type=Product::class))
+     *      )
+     * )
+     * @OA\Tag(name="Products")
+     * 
+     * @param Product $product
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
     #[Route('/api/products/{id}', name: 'product', methods: ['GET'])]
     public function getOneProduct(Product $product, SerializerInterface $serializer): JsonResponse
     {
